b456c28eb159e527561fe151c94914263ce71980
SamGu-NRX <sgu07966@gmail.com>
2024-07-12 15:23:23 -0500

    Fix fingerprint error

--- fingerprint.py
+++ fingerprint.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple, Dict
+import numpy as np
 
 def generate_fingerprints(peaks: List[Tuple[int, int]], fanout: int) -> List[int, Tuple[int, int, int]]:
     '''
@@ -16,22 +17,19 @@ def generate_fingerprints(peaks: List[Tuple[int, int]], fanout: int) -> List[int
 
     '''
     fingerprints = []
-
     peaks = np.array(peaks)
 
-    for i in enumerate(peaks): # for each peak
-        freq, absolute_time = peaks
-        distances = np.sqrt(np.sum((peaks[i:] - peaks[i])**2, axis=1))   
+    for i, peak in enumerate(peaks): # for each peak
+        freq, absolute_time = peak
+        distances = np.sqrt(np.sum((peaks[i:] - peak)**2, axis=1))   
         nearest_indices = np.argsort(distances)[1:fanout + 1]
-        
-        for n in nearest_indices:
-            new_freq, new_time = peaks[n]
-            if new_time > absolute_time:
+        for k in nearest_indices:
+            new_freq, new_time = peaks[k]
+            if new_time > absolute_time:    
                 delta_time = new_time - absolute_time
                 fingerprint = (freq, new_freq, delta_time)
-                # if fingerprint not in fingerprints:
-                fingerprints.append(absolute_time, fingerprint)
+                fingerprints.append((absolute_time, fingerprint))
 
     return fingerprints
 
-    #fingerprints = [(adsolute time1, (fm1, fn1, dt1)), (adsolute time2, (fm2, fn2, dt2)) ...]
+    # fingerprints = [(adsolute time1, (fm1, fn1, dt1)), (adsolute time2, (fm2, fn2, dt2)) ...]
